c685da708f17d8583dab452e670bcb407f174e63
Spencer Liao <sfliao@student.ubc.ca>
2025-02-04 23:24:51 -0800

    Quartz sync: Feb 4, 2025, 11:24 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,30 +33,30 @@ export const defaultContentPageLayout: PageLayout = {
       localGraph: {
         drag: true,
         zoom: true,
-        depth: 2, // Show up to 2 hops for more connections
+        depth: 1, // Show up to 2 hops for more connections
         scale: 1.5, // Slightly zoomed in for better clarity
         repelForce: 0.7, // More spacing between nodes
         centerForce: 0.5, // Stronger pull toward the center
         linkDistance: 50, // Spread nodes further apart
-        fontSize: 1, // Larger node labels
+        fontSize: 0.5, // Larger node labels
         opacityScale: 0.8, // Labels fade out more slowly
         removeTags: [], // No tags removed
-        showTags: true, // Show tags in the graph
-        enableRadial: true, // Constrain to a radial layout
+        showTags: false, // Show tags in the graph
+        enableRadial: false, // Constrain to a radial layout
       },
       globalGraph: {
         drag: true,
         zoom: true,
         depth: -1, // Unlimited depth for global graphs
         scale: 1.2, // Slightly zoomed out for global clarity
-        repelForce: 0.7, // More spacing for global view
+        repelForce: 0.5, // More spacing for global view
         centerForce: 0.5, // Keep global graph focused
         linkDistance: 50, // Spread nodes further apart
-        fontSize: 1, // Larger labels for global view
-        opacityScale: 0.8, // Labels fade out more slowly
+        fontSize: 0.5, // Larger labels for global view
+        opacityScale: 0.5, // Labels fade out more slowly
         removeTags: [], // No tags removed
-        showTags: true, // Show tags in global graph
-        enableRadial: true, // Constrain global graph to a radial layout
+        showTags: false, // Show tags in global graph
+        enableRadial: false, // Constrain global graph to a radial layout
       },    
     }),
     Component.DesktopOnly(Component.TableOfContents()),
