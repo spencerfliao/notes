68cac9fc2fa0121d7534631508aa3387572719c0
Spencer Liao <sfliao@student.ubc.ca>
2025-02-04 23:33:46 -0800

    Quartz sync: Feb 4, 2025, 11:33 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,28 +34,28 @@ export const defaultContentPageLayout: PageLayout = {
         drag: true, // whether to allow panning the view around
         zoom: true, // whether to allow zooming in and out
         depth: 1, // how many hops of notes to display
-        scale: 1.2, // default view scale
+        scale: 2, // default view scale
         repelForce: 0.5, // how much nodes should repel each other
         centerForce: 0.3, // how much force to use when trying to center the nodes
         linkDistance: 30, // how long should the links be by default?
         fontSize: 0.6, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
-        showTags: true, // whether to show tags in the graph
+        showTags: false, // whether to show tags in the graph
         enableRadial: false, // whether to constrain the graph, similar to Obsidian
       },
       globalGraph: {
         drag: true,
         zoom: true,
         depth: -1,
-        scale: 1,
-        repelForce: 0.5,
+        scale: 1.5,
+        repelForce: 0.6,
         centerForce: 0.3,
-        linkDistance: 30,
-        fontSize: 0.6,
+        linkDistance: 40,
+        fontSize: 0.2,
         opacityScale: 1,
         removeTags: [], // what tags to remove from the graph
-        showTags: true, // whether to show tags in the graph
+        showTags: false, // whether to show tags in the graph
         enableRadial: true, // whether to constrain the graph, similar to Obsidian
       },
     }),
