035f8769b491b504f058285c6e9feff88ab2ddf6
Spencer Liao <sfliao@student.ubc.ca>
2025-02-13 21:53:11 -0800

    Quartz sync: Feb 13, 2025, 9:53 PM

--- quartz/processors/filter.ts
+++ quartz/processors/filter.ts
@@ -1,3 +1,28 @@
+// import { BuildCtx } from "../util/ctx"
+// import { PerfTimer } from "../util/perf"
+// import { ProcessedContent } from "../plugins/vfile"
+
+// export function filterContent(ctx: BuildCtx, content: ProcessedContent[]): ProcessedContent[] {
+//   const { cfg, argv } = ctx
+//   const perf = new PerfTimer()
+//   const initialLength = content.length
+//   for (const plugin of cfg.plugins.filters) {
+//     const updatedContent = content.filter((item) => plugin.shouldPublish(ctx, item))
+
+//     if (argv.verbose) {
+//       const diff = content.filter((x) => !updatedContent.includes(x))
+//       for (const file of diff) {
+//         console.log(`[filter:${plugin.name}] ${file[1].data.slug}`)
+//       }
+//     }
+
+//     content = updatedContent
+//   }
+
+//   console.log(`Filtered out ${initialLength - content.length} files in ${perf.timeSince()}`)
+//   return content
+// }
+
 import { BuildCtx } from "../util/ctx"
 import { PerfTimer } from "../util/perf"
 import { ProcessedContent } from "../plugins/vfile"
@@ -6,8 +31,15 @@ export function filterContent(ctx: BuildCtx, content: ProcessedContent[]): Proce
   const { cfg, argv } = ctx
   const perf = new PerfTimer()
   const initialLength = content.length
+
   for (const plugin of cfg.plugins.filters) {
-    const updatedContent = content.filter((item) => plugin.shouldPublish(ctx, item))
+    let updatedContent = content.filter((item) => plugin.shouldPublish(ctx, item))
+
+    // Exclude everything inside the "private/" folder from being listed
+    updatedContent = updatedContent.filter((item) => {
+      const slug = item[1].data.slug || ""
+      return !slug.startsWith("private/") // Hide "private/" folder content
+    })
 
     if (argv.verbose) {
       const diff = content.filter((x) => !updatedContent.includes(x))
