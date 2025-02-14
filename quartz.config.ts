34f5d94f770200aa2f9f34c3265407f59742b0cd
Spencer Liao <sfliao@student.ubc.ca>
2025-02-13 22:17:44 -0800

    Quartz sync: Feb 13, 2025, 10:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private/**/*.md"],
+    ignorePatterns: ["_private/**/*.md"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
