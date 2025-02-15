6dd567338479b620bf278c2802e61c28b807a502
Spencer Liao <sfliao@student.ubc.ca>
2025-02-15 00:20:57 -0800

    Quartz sync: Feb 15, 2025, 12:20 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,36 +29,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph({
-      localGraph: {
-        drag: true, // whether to allow panning the view around
-        zoom: true, // whether to allow zooming in and out
-        depth: 1, // how many hops of notes to display
-        scale: 4, // default view scale
-        repelForce: 0.5, // how much nodes should repel each other
-        centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 30, // how long should the links be by default?
-        fontSize: 1, // what size should the node labels be?
-        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
-        removeTags: [], // what tags to remove from the graph
-        showTags: false, // whether to show tags in the graph
-        enableRadial: false, // whether to constrain the graph, similar to Obsidian
-      },
-      globalGraph: {
-        drag: true,
-        zoom: true,
-        depth: -1,
-        scale: 1.5,
-        repelForce: 0.6,
-        centerForce: 0.3,
-        linkDistance: 40,
-        fontSize: 0.35,
-        opacityScale: 1,
-        removeTags: [], // what tags to remove from the graph
-        showTags: false, // whether to show tags in the graph
-        enableRadial: true, // whether to constrain the graph, similar to Obsidian
-      },
-    }),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
